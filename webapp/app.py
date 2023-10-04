44cbe0a539f53df4e67b655717bc39ea3cd3081d
Dinesh Jonnalagadda <106725225+Dineshjnld@users.noreply.github.com>
2023-10-04 14:14:56 +0530

    Update app.py

--- webapp/app.py
+++ webapp/app.py
@@ -77,9 +77,13 @@ def production_mode():
     st.markdown(hide_streamlit_style, unsafe_allow_html=True)
 
 if __name__ == '__main__':
-    img = Image.open(Path(BASE_DIR) / 'imgs/logo.png')
-    st.set_page_config(page_title='AI Learning Catalysts',
-                    layout='wide')
+     img_path = 'webapp/static/imgs/logo.png'  # Provide the direct path to your image file
+    img = Image.open(img_path)
+
+    st.set_page_config(
+        page_title='AI Learning Catalysts',
+        page_icon=img,
+        layout='wide')
 
     write_header()
     write_ui()
