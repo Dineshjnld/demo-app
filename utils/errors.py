665c3a9a0446134a588263e23c13834dbc52561a
dineshjnld <dineshjnld22@gmail.com>
2023-10-04 13:15:17 +0530

    new

--- utils/errors.py
+++
@@ -1,9 +0,0 @@
-"""class for handling errors"""
-
-
-class ResourceNotFoundException(Exception):
-    """Class for custom Exceptions"""
-
-    def __init__(self, message="Resource not found"):
-        self.message = message
-        super().__init__(self.message)
