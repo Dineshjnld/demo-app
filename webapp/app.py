4dd9748dd43ca8a73264f6d1d06808154e8e583f
Dinesh Jonnalagadda <106725225+Dineshjnld@users.noreply.github.com>
2023-10-07 16:13:12 +0530

    Update app.py

--- webapp/app.py
+++ webapp/app.py
@@ -3,9 +3,16 @@ import streamlit as st
 # Define a dictionary of valid usernames and passwords (you should replace these with your actual credentials)
 valid_credentials = {"user": "training"}
 
+from PIL import Image
+from pathlib import Path
+from config import BASE_DIR
+from ui.ui_manager import *
+from utils.logging_handler import Logger
+
 def write_login_page():
     """Displays a login page with username and password input fields.
     """
+    
     st.title(':red[Login]')
     username = st.text_input("Username")
     password = st.text_input("Password", type="password")
@@ -22,15 +29,18 @@ def validate_credentials(username, password):
     """
     return valid_credentials.get(username) == password
 
-def display_courses():
-    """Displays a list of available courses to the user.
+def write_header():
+    """Writes the header part of the UI.
     """
-    st.title("Available Courses")
-    st.write("Course 1: Introduction to AI")
-    st.write("Course 2: Machine Learning Fundamentals")
-    st.write("Course 3: Deep Learning Essentials")
-    # Add more courses as needed
+    st.title(':blue[AI Based IT Training System]')
 
+def write_footer():
+    """Writes the footer part of the UI.
+    """
+    
+    st.sidebar.warning(':blue[Please note that this tool is only for demo purpose]')
+    st.sidebar.image("webapp/static/imgs/logo.png", use_column_width=True)
+    st.sidebar.warning(':blue[AI Based Training System]')
 def write_ui():
     """Handles the major part of the UI.
     """
@@ -38,12 +48,42 @@ def write_ui():
         # Display the login page if the user has not logged in
         write_login_page()
     else:
-        # User has logged in, display the courses or course information
-        display_courses()  # Display available courses
+        # User has logged in, display the rest of the UI
+        if "viva_mode" in st.session_state:
+            display_course_banner(st.session_state["course_selected"])
+            st.markdown("---")
+            display_viva_chat_bot(st.session_state["course_selected"])
+        elif "course_selected" not in st.session_state:
+            st.markdown("---")
+            display_courses()
+        elif "video_selected" not in st.session_state:
+            display_course_banner(st.session_state["course_selected"])
+            display_video_tabs(st.session_state["course_selected"])
+        else:
+            st.markdown("---")
+            video_selected = st.session_state["video_selected"]
+            display_video_content(Path(video_selected))
+            display_qa_chat_bot()
+
+def production_mode():
+    hide_streamlit_style = """
+    <style>
+    MainMenu {visibility: hidden;}
+    footer {visibility: hidden;}
+    a[class^="viewerBadge_container*"]  {visibility: hidden;}
+    </style>
+    """
+    st.markdown(hide_streamlit_style, unsafe_allow_html=True)
 
 if __name__ == '__main__':
+    img_path = 'webapp/static/imgs/logo.png'  # Provide the direct path to your image file
+    img = Image.open(img_path)
+
     st.set_page_config(
         page_title='AI Learning Catalysts',
+        page_icon=img,
         layout='wide')
 
+    write_header()
     write_ui()
+    write_footer()
